5d9e2a2817763b5a9d9935fc7b3e772dad7fa806
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-09-10 17:39:12 +0100

    add change to implementation file url

--- Controllers/InfoController.cs
+++ Controllers/InfoController.cs
@@ -19,7 +19,7 @@ namespace HngX.StageOne.Controllers
                 CurrentDay = DateTime.UtcNow.DayOfWeek.ToString(),
                 UtcTime = currentUtcTime.ToString("yyyy-MM-ddTHH:mm:ssZ"),
                 Track = track,
-                GithubFileUrl = "https://github.com/IEdiong/hngx-backend-s1/blob/main/Program.cs",
+                GithubFileUrl = "https://github.com/IEdiong/hngx-backend-s1/blob/main/Controllers/InfoController.cs",
                 GithubRepoUrl = "https://github.com/IEdiong/hngx-backend-s1",
                 StatusCode = 200
             };
